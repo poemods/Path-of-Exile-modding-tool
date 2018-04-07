38f6fce647bc615fb731467b1dd5d3edc3c7a355
poemods <user@example.com>
2018-04-07 11:32:51 +0200

    ao correction

--- mods/ao.py
+++ mods/ao.py
@@ -17,22 +17,20 @@ masterfilter_exclude=[
     ]
 
 condition=[
-   "AnimationController",
-   #"Hull",
-   #"AttachedAnimatedObjects",
-   ]
+    #"AnimationController",
+    "Hull",
+    #"AttachedAnimatedObjects",
+    ]
 
 def execute(filename, backupfiledata, modifyggpk):
-   filedata, encoding, bom = modifyggpk.stringcleanup(backupfiledata, "UTF-16-LE")
-   filedatamod=filedata
-   mi=re.finditer(r'((\w+)[\t\r\n ]*\{.*?\})', filedata, flags=re.DOTALL)
-   for mii in mi :
-      tagis=mii.group(2)
-      found=False
-      for cond in condition :
-         if cond==tagis :
-            found=True
-      if found is False :
-         filedatamod=re.sub(tagis+r'[\t\r\n ]*\{.*?\}', tagis+r'\r\n{\r\n}', filedatamod, flags=re.DOTALL)
-   return filedatamod, encoding, bom
+    filedata, encoding, bom = modifyggpk.stringcleanup(backupfiledata, "UTF-16-LE")
+    filedatamod=re.sub(r'Hull[\t\r\n ]*\{.*?\}', r'Hull\r\n{\r\n}', filedata, flags=re.DOTALL)
+#     filedatamod=filedata
+#     mi=re.finditer(r'((\w+)[\t\r\n ]*\{.*?\})', filedatamod, flags=re.DOTALL)
+#     for mii in mi :
+#         tagis=mii.group(2)
+#         for cond in condition :
+#             if cond==tagis :
+#                 filedatamod=re.sub(tagis+r'[\t\r\n ]*\{.*?\}', tagis+r'\r\n{\r\n}', filedatamod, flags=re.DOTALL)
+    return filedatamod, encoding, bom
 
